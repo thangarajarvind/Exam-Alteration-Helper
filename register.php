e4d4a0c6c3a1f18fa648e1cdc1ce30023fae530d
unknown <vidyarmenon123@gmail.com>
2021-03-14 19:34:20 +0530

    password match

--- register.php
+++ register.php
@@ -73,6 +73,21 @@ else{
         if ($rnum!=0) {
       echo "Someone already register using this email";
     }
+    $query=mysql_query("SELECT * FROM register WHERE email='".$email."' AND pass1='".$pass1."'");
+      $numrows=mysql_num_rows($query);  
+      if($numrows!=0)  
+      {  
+        while($row=mysql_fetch_assoc($query))  
+        {  
+          $dbemail=$row['email'];  
+          $dbpassword1=$row['pass1'];  
+          
+        if($email == $dbemail && md5($pass1)==md5($dbpassword1))
+        {
+           echo "success";
+        } 
+      } 
+    } 
      $stmt->close();
      $conn->close();
     }
