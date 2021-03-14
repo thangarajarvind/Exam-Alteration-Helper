585227a3bca1c287fe9aab4e8177390b09d6c3ea
Arvind <thangarajarvind@gmail.com>
2021-03-14 21:07:26 +0530

    Revert "Meh"
    This reverts commit 14374146cc4a4928aa92f7e324a6861f4ac02821.

--- register.php
+++ register.php
@@ -7,7 +7,27 @@ $pass1 = $_POST['pass1'];
 $pass2 = $_POST['pass2'];
 $dep = $_POST['dep'];
 
-$code= rand(10000,99999);
+$id="";
+$id_num = (string)random_int ( 100 , 999 );
+$id = $dep.$id_num;
+
+$check = NULL;
+$check = "SELECT ISNULL(
+  (
+     SELECT id 
+     FROM register 
+     WHERE id = $id
+  ), NULL)";
+while($check != NULL){
+  $id_num = (string)random_int ( 100 , 999 );
+  $id = $dep.$id_num;
+  $check = "SELECT (SELECT ISNULL(
+    (
+       SELECT id 
+       FROM register 
+       WHERE id = $id
+    ), NULL)";
+}
 
 if (!empty($name) || !empty($email) || !empty($mno) || !empty($pass1) || !empty($pass2) || !empty($dep) )
 {
@@ -26,7 +46,7 @@ if (mysqli_connect_error()){
 }
 else{
   $SELECT = "SELECT email From register Where email = ? Limit 1";
-  $INSERT = "INSERT Into register (name , email ,mno, pass1, pass2, dep, code )values(?,?,?,?,?,?,?)";
+  $INSERT = "INSERT Into register (name , email ,mno, pass1, pass2, dep, id )values(?,?,?,?,?,?,?)";
 
 //Prepare statement
      $stmt = $conn->prepare($SELECT);
@@ -38,14 +58,36 @@ else{
 
      //checking username
       if ($rnum==0) {
-      $stmt->close();
-      $stmt = $conn->prepare($INSERT);
-      $stmt->bind_param("sssssss", $name , $email ,$mno, $pass1, $pass2, $dep, $code);
-      $stmt->execute();
-      echo "New record inserted sucessfully";
-     } else {
+        if ($pass1==$pass2) {
+          $pass1=$pass2=md5($pass1);
+          $stmt->close();
+          $stmt = $conn->prepare($INSERT);
+          $stmt->bind_param("sssssss", $name , $email ,$mno, $pass1, $pass2, $dep, $id);
+          $stmt->execute();
+          echo "New record inserted sucessfully";
+        }
+        else {
+          echo "Passwords mismatch";
+        }
+      } 
+        if ($rnum!=0) {
       echo "Someone already register using this email";
-     }
+    }
+    $query=mysql_query("SELECT * FROM register WHERE email='".$email."' AND pass1='".$pass1."'");
+      $numrows=mysql_num_rows($query);  
+      if($numrows!=0)  
+      {  
+        while($row=mysql_fetch_assoc($query))  
+        {  
+          $dbemail=$row['email'];  
+          $dbpassword1=$row['pass1'];  
+          
+        if($email == $dbemail && md5($pass1)==md5($dbpassword1))
+        {
+           echo "success";
+        } 
+      } 
+    } 
      $stmt->close();
      $conn->close();
     }
