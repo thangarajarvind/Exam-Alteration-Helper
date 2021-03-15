bc5db6c73bcad0e3be7b15c913f2cb8b44dc0a11
Arvind <thangarajarvind@gmail.com>
2021-03-15 22:12:59 +0530

    validation

--- login.php
+++ login.php
@@ -23,7 +23,6 @@ if (mysqli_connect_error()){
 
 else{
     $SELECT = "SELECT email From register Where email = ? Limit 1";
-    $PASSWORD = "SELECT pass1 From register Where email = ?";
 
     $stmt = $conn->prepare($SELECT);
     $stmt->bind_param("s", $user);
@@ -34,15 +33,15 @@ else{
     $stmt->close();
 
     if ($rnum==1) {
-        $stmt = $conn->prepare($PASSWORD);
-        $stmt->bind_param("s", $user);
-        $stmt->execute();
-        $stmt->bind_result($email);
-        $stmt->store_result();
+        $result = mysqli_query($conn,"SELECT * FROM register where email='" . $_POST['user'] . "'");
+        $row = mysqli_fetch_assoc($result);
+        $dbpass = $row['pass1'];
+        $pass = md5($pass);
+        if($pass == $dbpass){
+            echo 'Login Success';
+        }
+        else{
+            echo 'Incorrect';
+        }
     }
-
-
-
-
-	echo $sql;
 	}
