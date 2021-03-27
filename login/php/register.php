d9dab7fc195897c6bf61bf2e8ae07eb85a6186ec
Arvind <thangarajarvind@gmail.com>
2021-03-28 01:16:17 +0530

    Duplication of ID will be avoided

--- login/php/register.php
+++ login/php/register.php
@@ -33,8 +33,29 @@ if (mysqli_connect_error()){
 else{
   $SELECT = "SELECT email From register Where email = ? Limit 1";
   $SELECTM = "SELECT mno From register Where mno = ? Limit 1";
+  $ID_CHECK = "SELECT id From register Where id = ?";
   $INSERT = "INSERT Into register (name , email ,mno, pass1, pass2, dep, id, code )values(?,?,?,?,?,?,?,?)";
 //Prepare statement
+    $stmt = $conn->prepare($ID_CHECK);
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
+    $stmt->bind_result($id);
+    $stmt->store_result();
+    $idrow = $stmt->num_rows;
+    $stmt->close();
+    
+    while($idrow>1){
+        $id_num = (string)random_int ( 100 , 999 );
+        $id = $dep.$id_num;
+        $stmt = $conn->prepare($ID_CHECK);
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $stmt->bind_result($id);
+        $stmt->store_result();
+        $idrow = $stmt->num_rows;
+        $stmt->close();
+      }
+
      $stmt = $conn->prepare($SELECT);
      $stmt->bind_param("s", $email);
      $stmt->execute();
@@ -99,5 +120,4 @@ else{
 function popup ($m,$l){
   echo "<script type='text/javascript'>alert('$m');window.location.href = '$l';window.close();</script>";
 }
-
 ?>
